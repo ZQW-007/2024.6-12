b66aafa4ad9c161d8afcce0dcc22b9935b2f4862
张清闻 <3112931437@qq.com>
2024-09-21 20:11:14 +0800

    es6第一天： 1.es6->es5的转换； 2.es6模块化和common.js的区别可以参考https://segmentfault.com/a/1190000039346572

---
+++ es6DAY01/01.js
@@ -0,0 +1,9 @@
+const b = 2;
+let c = 3;
+const d  = 3;
+export {b,c ,d};
+
+// modules.exports = {
+//     b: '2',
+//     c: '3'
+// };
---
+++ es6DAY01/02.js
@@ -0,0 +1,5 @@
+import {b,c,d} from './01.js';
+console.log(b,c,d);
+
+// const {b,c} = require('./01.js');
+// console.log(b,c);
